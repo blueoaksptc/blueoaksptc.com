8f11d31e89b29d26e1d687b8947854e159878106
Mike Pontillo <mpontillo@mocana.com>
2014-08-13 12:42:21 -0700

    Use "pip install --target" to install pyjade
     * Prevents a bug where pip will install pyjade's dependencies elsewhere

--- scripts/pyjade.sh
+++ scripts/pyjade.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+
+#
+# By sourcing this script, you will ensure that pyjade is installed,
+# and available on the PYTHONPATH.
+#
+
+ORIG_PWD="$(pwd)"
 
 PYTHON_LIBRARY_DIRECTORIES="lib /usr/local/lib"
 
@@ -12,8 +19,23 @@ PYTHON_VERSION_REGEX=$(echo $PYTHON_VERSION | sed 's/\./\\./g')
 # echo $PYTHON_VERSION_REGEX
 
 # Install pyjade to the current directory
-if [ ! -x bin/pyjade ]; then
-    pip install --install-option="--prefix=$(pwd)" pyjade
+if [ ! -d lib/pyjade ]; then
+    # Workaround for a Homebrew bug that prevents "pip install --target" from working:
+    # http://stackoverflow.com/questions/24257803/
+    if [ ! -f ~/.pydistutils.cfg ]; then
+    cat << EOF > ~/.pydistutils.cfg
+[install]
+prefix=
+EOF
+    fi
+    pip install --target="$(pwd)/lib" pyjade
 fi
 
-export PYTHONPATH=$(find $PYTHON_LIBRARY_DIRECTORIES -type d -name site-packages | grep "python${PYTHON_VERSION_REGEX}/" | xargs -n 1 ./get_path | tr '\n' ':' | sed 's/:$//')
+# Previous version which used:
+# pip install --install-option="--prefix=$(pwd)" pyjade
+# (this doesn't handle dependencies)
+#export PYTHONPATH=$(find $PYTHON_LIBRARY_DIRECTORIES -type d -name site-packages | grep "python${PYTHON_VERSION_REGEX}/" | xargs -n 1 ./get_path | tr '\n' ':' | sed 's/:$//')
+
+export PYTHONPATH=$(./get_path lib)
+
+cd "$ORIG_PWD"
