53d5db315f70b7218d3702a57dee71cb31e3c789
Mike Pontillo <mpontillo@mocana.com>
2014-08-13 11:02:22 -0700

    Make parse.py more "pythonic"
     * Create a separate main() function

--- scripts/parse.py
+++ scripts/parse.py
@@ -17,26 +17,33 @@ def get_script_directory():
 
 os.chdir(get_script_directory())
 
-with open('tmp/blueoaks.jade') as f:
-    lines = f.readlines()
-    index = 0
-    start_index = -1
-    for line in map(str.strip, lines):
-        # print line
-        if line == 'ul.fbList':
-            start_index = index
-            break
-        index = index + 1
-    # print "Found start index: ", start_index
-    lines = lines[start_index:]
-    # print lines
-    starting_whitespace = get_leading_whitespace(lines[0])
-    index = 1
-    for line in lines[1:]:
-        if get_leading_whitespace(line) <= starting_whitespace:
-            break
-        index = index + 1
-    # print "Found end index: ", index
-    lines = lines[0:index]
-    for line in lines:
-        print line.rstrip()[starting_whitespace:]
+def print_relevant_lines(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+        index = 0
+        start_index = -1
+        for line in map(str.strip, lines):
+            # print line
+            if line == 'ul.fbList':
+                start_index = index
+                break
+            index = index + 1
+        # print "Found start index: ", start_index
+        lines = lines[start_index:]
+        # print lines
+        starting_whitespace = get_leading_whitespace(lines[0])
+        index = 1
+        for line in lines[1:]:
+            if get_leading_whitespace(line) <= starting_whitespace:
+                break
+            index = index + 1
+        # print "Found end index: ", index
+        lines = lines[0:index]
+        for line in lines:
+            print line.rstrip()[starting_whitespace:]
+
+def main():
+    print_relevant_lines('tmp/blueoaks.jade')
+
+if __name__ == "__main__":
+    main()
